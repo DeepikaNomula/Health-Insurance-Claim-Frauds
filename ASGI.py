2bdb748361867d2fb2714e85b21e853cb671b3be
Deepika Nomula <116021035+DeepikaNomula@users.noreply.github.com>
2023-11-19 11:03:57 +0530

    Delete ASGI.py

--- ASGI.py
+++
@@ -1,7 +0,0 @@
-importos
-
-fromdjango.core.asgiimportget_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'identifying_health_insurance_claim_frauds.settings')
-
-application=get_asgi_application()
