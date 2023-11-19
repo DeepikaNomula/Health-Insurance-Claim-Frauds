dca208c5a2e1b5e2cb1ffdf8557e1a853727bb33
Deepika Nomula <116021035+DeepikaNomula@users.noreply.github.com>
2023-11-19 11:04:32 +0530

    Delete WSGI.py

--- WSGI.py
+++
@@ -1,7 +0,0 @@
-
-importos
-
-fromdjango.core.wsgiimportget_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'identifying_health_insurance_claim_frauds.settings')
-application=get_wsgi_application()
