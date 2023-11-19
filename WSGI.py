7f6cf1b1b3bb0136f49f454d88f28ddea6ed60e8
Deepika Nomula <116021035+DeepikaNomula@users.noreply.github.com>
2023-11-19 10:52:03 +0530

    Django settings modules

---
+++ WSGI.py
@@ -0,0 +1,7 @@
+
+importos
+
+fromdjango.core.wsgiimportget_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'identifying_health_insurance_claim_frauds.settings')
+application=get_wsgi_application()
