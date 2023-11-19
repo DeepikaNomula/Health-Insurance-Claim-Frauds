a224095fc5da754b5fbdbcb5f7b6a17d269a7556
Deepika Nomula <116021035+DeepikaNomula@users.noreply.github.com>
2023-11-19 11:05:31 +0530

    Django

---
+++ templates/ASGI.PY
@@ -0,0 +1,7 @@
+importos
+
+fromdjango.core.asgiimportget_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'identifying_health_insurance_claim_frauds.settings')
+
+application=get_asgi_application()
